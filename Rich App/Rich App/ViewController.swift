732a58e746dc8660b0d332c2198301ba4c30e992
Jason Lee <jasonlee@jasons-mac-mini.myfiosgateway.com>
2020-12-31 00:18:06 -0500

    init app-richapp

---
+++ Rich App/Rich App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rich App
+//
+//  Created by Jason Lee on 12/31/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
