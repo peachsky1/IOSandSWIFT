fb0f53f21f687d4e285e39b8ab0787935fb2e450
Jason Lee <jasonlee@jasons-mac-mini.myfiosgateway.com>
2021-01-01 23:50:15 -0500

    adding buttons and images for link

--- DiceApp/Dicee-iOS13/ViewController.swift
+++ DiceApp/Dicee-iOS13/ViewController.swift
@@ -9,12 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+//  Interface Builder Outlet allows me to reference a UI elem.
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
+//    var diceList = [#imageLiteral(resourceName: <#T##String#>),#imageLiteral(resourceName: <#T##String#>),#imageLiteral(resourceName: <#T##String#>)]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        diceImageViewOne.image = #imageLiteral(resourceName: "DiceTwo")
+        diceImageViewTwo.image = #imageLiteral(resourceName: "DiceTwo")
+        
+        diceImageViewOne.alpha = 0.5
+        diceImageViewTwo.alpha = 0.5
     }
-
-
+// Interface Builder Action allows me to reference the source code
+    @IBAction func rollButton(_ sender: UIButton) {
+        print("Button got tapped!")
+        if diceImageViewOne.alpha != 1 {
+            diceImageViewOne.alpha = 1
+            diceImageViewTwo.alpha = 1
+        }
+        diceImageViewOne.image = #imageLiteral(resourceName: "DiceFour")
+        diceImageViewTwo.image = #imageLiteral(resourceName: "DiceSix")
+    }
+    
 }
 
