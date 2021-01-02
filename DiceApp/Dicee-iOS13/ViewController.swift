80918e073ccb9479b7cecc7b7186b152c8f97a41
Jason Lee <jasonlee@jasons-mac-mini.myfiosgateway.com>
2021-01-02 00:00:23 -0500

    added roll button for random dice

--- DiceApp/Dicee-iOS13/ViewController.swift
+++ DiceApp/Dicee-iOS13/ViewController.swift
@@ -12,28 +12,36 @@ class ViewController: UIViewController {
 //  Interface Builder Outlet allows me to reference a UI elem.
     @IBOutlet weak var diceImageViewOne: UIImageView!
     @IBOutlet weak var diceImageViewTwo: UIImageView!
+//    let: when u dont wanna change the context. var = variable.
+    let diceList = [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")]
     
-//    var diceList = [#imageLiteral(resourceName: <#T##String#>),#imageLiteral(resourceName: <#T##String#>),#imageLiteral(resourceName: <#T##String#>)]
-    
+//    random number generator = Int.random(in: 1...10)
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
 
-        diceImageViewOne.image = #imageLiteral(resourceName: "DiceTwo")
-        diceImageViewTwo.image = #imageLiteral(resourceName: "DiceTwo")
+        diceImageViewOne.image = diceList[randomNumberGenerator()]
+        diceImageViewTwo.image = diceList[randomNumberGenerator()]
         
         diceImageViewOne.alpha = 0.5
         diceImageViewTwo.alpha = 0.5
     }
 // Interface Builder Action allows me to reference the source code
     @IBAction func rollButton(_ sender: UIButton) {
+        
         print("Button got tapped!")
         if diceImageViewOne.alpha != 1 {
             diceImageViewOne.alpha = 1
             diceImageViewTwo.alpha = 1
         }
-        diceImageViewOne.image = #imageLiteral(resourceName: "DiceFour")
-        diceImageViewTwo.image = #imageLiteral(resourceName: "DiceSix")
+        diceImageViewOne.image = diceList[randomNumberGenerator()]
+//        diceImageViewTwo.image = diceList[randomNumberGenerator()]
+//        randomElement
+        diceImageViewTwo.image = diceList.randomElement()
+        
+    }
+    func randomNumberGenerator() -> Int {
+        return Int.random(in: 0...5)
     }
     
 }
